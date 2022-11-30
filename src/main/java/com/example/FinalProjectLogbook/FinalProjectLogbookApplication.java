fc92fff320eafa7b55690442a8bd15b96ff16a40
DANIEL CHAUVIERE <bgq007@myy.haaga-helia.fi>
2022-12-01 00:42:57 +0200

    final

--- src/main/java/com/example/FinalProjectLogbook/FinalProjectLogbookApplication.java
+++ src/main/java/com/example/FinalProjectLogbook/FinalProjectLogbookApplication.java
@@ -35,18 +35,20 @@ public class FinalProjectLogbookApplication {
 	public CommandLineRunner flightDemo(FlightRepository repository, AircraftRepository crepo, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of flights");
+			//Adding different aircraft registrations
 			crepo.save(new Aircraft("OY-JZM"));
 			crepo.save(new Aircraft("OY-JZN"));
 			crepo.save(new Aircraft("OY-JTP"));
 			
-			
+			//adding a few flights
 			repository.save(new Flight(LocalDate.of(2021, 7, 12), "EFHK", LocalTime.of(13,12), "EFVA", LocalTime.of(14,12), LocalTime.of(01,00), 1, "Self", "Training flight", crepo.findByName("OY-JZM").get(0)));
 			repository.save(new Flight(LocalDate.of(2021, 7, 13), "EFVA", LocalTime.of(10,00), "EKCH", LocalTime.of(12,00), LocalTime.of(02,00), 2, "Self", "Ferry flight", crepo.findByName("OY-JZN").get(0)));
 
-			
+			//adding a admin user, password is adminpassword
 			User admin = new User("admin", "$2a$04$eC7eQbfku.zj8ST9lkuZEubrnGi7iYtrU3s.mrO.xwpBY3Ym5om.W", "ADMIN", "");
 			urepository.save(admin);
 			
+			//adding a test user, password is password
 			User user1 = new User("user", "$2a$04$d2VDhQv3tLUBiR6Zp.pfOORsLeR/Qqqpk/bknRuPHKMUGsA1sGC3O", "USER", "");
 			urepository.save(user1);
 
